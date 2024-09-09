ddad454aebe512bcd7b1bf4c6e541f867a8e6aa6
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-09-09 13:27:26 +0200

    Initial commit, basic server setup

---
+++ server/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+    res.send("OK")
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
