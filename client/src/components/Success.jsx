cffd9089246d41370714e2305c102f38a7ab3a7d
unknown <faridwilhelmzimmermann1111@gmail.com>
2025-01-31 13:14:36 +0100

    added anchor to improve user experience

--- client/src/components/Success.jsx
+++ client/src/components/Success.jsx
@@ -2,10 +2,11 @@ import "./Success.css";
 
 
 export default function Success({returnObj}) {
+    const urlStr = `${window.location}api/shorturl/${returnObj.shortenedUrl}`
 
     return (
         <div className="success">
-            <p>Success! You can access your URL @ {`${window.location}/api/shorturl/${returnObj.shortenedUrl}`}</p>
+            <p>Success! You can access your URL @ <a href={urlStr}>{urlStr}</a></p>
         </div>
     )
 }
