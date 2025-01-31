4bd20b85254379647f7af6ebf084a18bd55cef6f
unknown <faridwilhelmzimmermann1111@gmail.com>
2025-01-31 13:10:12 +0100

    changed bug regarding correct url display

--- client/src/components/Success.jsx
+++ client/src/components/Success.jsx
@@ -5,7 +5,7 @@ export default function Success({returnObj}) {
 
     return (
         <div className="success">
-            <p>Success! You can access your URL @ {`localhost:3000/api/shorturl/${returnObj.shortenedUrl}`}</p>
+            <p>Success! You can access your URL @ {`${window.location}/api/shorturl/${returnObj.shortenedUrl}`}</p>
         </div>
     )
 }
