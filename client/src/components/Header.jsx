afe0ad1227b41f3923841056346f77f6dacb47cb
Farid <faridwilhelmzimmermann1111@gmail.com>
2025-01-31 18:08:28 +0100

    Update Header.jsx for better styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(props) {
 
     return (
         <div className="header">
-            <h2>Url Shortener</h2>
+            <h1>Url Shortener</h1>
             <p>Sample usage: <em>https://spotify.com</em></p>
             <p><em>Don´t forget the https/http protocol</em></p>
         </div>
